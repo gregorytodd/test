daa097b712f3e18551f99d67793f3151ed4d5d9b
gregtodd <gregoryjtodd@gmail.com>
2014-10-29 12:17:39 +0000

    changes sorry for bad code

--- ListHelper.java
+++ ListHelper.java
@@ -2,13 +2,9 @@ package some.sample.code;
 
 public class ListHelper {
 	
-	public getArrayLength(List list) {
-		int length= 0;
-	
-		for(int idx = 0; idx < list.length; idx++){
-			length++;
-		}
-	
-		System.out.println("length is : " + length);
+	public getListLength(List list) {
+			
+		return list.size();
+		
 	}
 }
